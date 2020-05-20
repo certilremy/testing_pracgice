690878624a043fdec1fbc09a1151d8d003f7475d
certilremy <certilremy@gmail.com>
2020-05-20 16:24:11 -0400

    add test for array Alalyser

--- ArrayAnalysis.test.js
+++ ArrayAnalysis.test.js
@@ -1,1 +1,18 @@
-import arrayAnalysis from './index';
+import { arrayAnalysis } from './index';
+
+
+test('find Min', () => {
+  expect(arrayAnalysis.analyze([2, 4, 6, 3, 6]).min).toBe(2);
+});
+
+test('find Max', () => {
+  expect(arrayAnalysis.analyze([2, 4, 6, 3, 6]).max).toBe(6);
+});
+
+test('find Average', () => {
+  expect(arrayAnalysis.analyze([2, 4, 6, 3, 6]).average).toBe(4.2);
+});
+
+test('find Length', () => {
+  expect(arrayAnalysis.analyze([2, 4, 6, 3, 6]).length).toBe(5);
+});

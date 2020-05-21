dd3bc2aa8068fc4972c114b068ba76423e5352b5
certilremy <certilremy@gmail.com>
2020-05-21 09:28:10 -0400

    add more test for string manipulation

--- stringManipulator.test.js
+++ stringManipulator.test.js
@@ -1,9 +1,25 @@
 import { workString } from './index';
 
 test('Capitalize string', () => {
-  expect(workString.capitalize('hello')).toEqual('HELLO');
+  expect(workString.capitalize('hello')).toEqual('Hello');
+});
+
+test('Return error if empy sring given', () => {
+  expect(workString.capitalize('')).toEqual('empty string not allowed!');
+});
+
+test('Return error if non sring given', () => {
+  expect(workString.capitalize(1)).toEqual('Only string allowed');
 });
 
 test('Reverse string', () => {
   expect(workString.reverseString('hello')).toEqual('olleh');
+});
+
+test('Return error if empy sring given', () => {
+  expect(workString.reverseString('')).toEqual('Empty string not allowed!');
+});
+
+test('Return error if non sring given', () => {
+  expect(workString.reverseString(1)).toEqual('Only string allowed!');
 });
